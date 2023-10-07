f12853b8f4a0b2e824636029d383871eeca37950
leutnant <karl.shtolz.army@gmail.com>
2023-10-07 15:12:19 +0300

    39. Combination Sum

---
+++ src/main/java/org/example/solutions/CombinationSum.java
@@ -0,0 +1,54 @@
+package org.example.solutions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 39. Combination Sum
+ * Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations
+ * of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+ * The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the
+ * frequency
+ *  of at least one of the chosen numbers is different.
+ * The test cases are generated such that the number of unique combinations that sum up to target is less than 150
+ * combinations for the given input.
+ *
+ * Example 1:
+ * Input: candidates = [2,3,6,7], target = 7
+ * Output: [[2,2,3],[7]]
+ * Explanation:
+ * 2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+ * 7 is a candidate, and 7 = 7.
+ * These are the only two combinations.
+ *
+ * Example 2:
+ * Input: candidates = [2,3,5], target = 8
+ * Output: [[2,2,2,2],[2,3,3],[3,5]]
+ *
+ * Example 3:
+ * Input: candidates = [2], target = 1
+ * Output: []
+ */
+public class CombinationSum {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        solve(candidates, target, 0, 0, new ArrayList<>(), res);
+        return res;
+    }
+
+    void solve(int[] candidates, int target, int sum, int start, List<Integer> temp, List<List<Integer>> res) {
+        if(sum == target) {
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+        if(sum > target) {
+            return;
+        }
+
+        for(int i = start; i < candidates.length; i++) {
+            temp.add(candidates[i]);
+            solve(candidates, target, sum + candidates[i], i, temp, res);
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
