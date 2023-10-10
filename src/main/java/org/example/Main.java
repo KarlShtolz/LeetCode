671e87574788104b558cb1884832c6dea9470a4e
leutnant <karl.shtolz.army@gmail.com>
2023-10-10 03:47:01 +0300

    43, 44, 45

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,12 @@
 package org.example;
 
+import org.example.solutions.JumpGameII;
 import org.example.solutions.TrappingRainWater;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int [] arr = new int[]{0,1,0,2,1,0,1,3,2,1,2,1};
-        TrappingRainWater trappingRainWater = new TrappingRainWater();
-        System.out.println(trappingRainWater.trap(arr));
-        arr = new int[]{4,2,0,3,2,5};
-//        System.out.println(trappingRainWater.trap(arr));
+
     }
 }
