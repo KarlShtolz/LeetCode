ec9ca1d982722ca5dd634080fba4cda60fd26f8c
leutnant <karl.shtolz.army@gmail.com>
2023-10-10 04:07:54 +0300

    234. Palindrome Linked List

---
+++ src/main/java/org/example/solutions/PalindromeLinkedList.java
@@ -0,0 +1,58 @@
+package org.example.solutions;
+
+/**
+ * 234. Palindrome Linked List
+ * Given the head of a singly linked list, return true if it is a
+ * palindrome
+ *  or false otherwise.
+ *
+ * Example 1:
+ * Input: head = [1,2,2,1]
+ * Output: true
+ *
+ * Example 2:
+ * Input: head = [1,2]
+ * Output: false
+ */
+
+public class PalindromeLinkedList {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode rev = reverse(slow.next);
+        slow = head;
+        fast = rev;
+        while (fast != null) {
+            if (slow.val != fast.val) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return true;
+    }
+    private ListNode reverse(ListNode node) {
+        ListNode reverse = new ListNode(-1);
+        while (node != null) {
+            ListNode next = node.next;
+            node.next = reverse.next;
+            reverse.next = node;
+            node = next;
+        }
+        return reverse.next;
+    }
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
