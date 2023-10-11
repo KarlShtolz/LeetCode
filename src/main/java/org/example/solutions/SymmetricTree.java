ada3eefd6d65cfdc8d7d7f75f94a14b1c2ca3e32
leutnant <karl.shtolz.army@gmail.com>
2023-10-12 00:04:56 +0300

    101. Symmetric Tree

---
+++ src/main/java/org/example/solutions/SymmetricTree.java
@@ -0,0 +1,46 @@
+package org.example.solutions;
+
+/**
+ * 101. Symmetric Tree
+ * Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+ *
+ * Example 1:
+ * Input: root = [1,2,2,3,4,4,3]
+ * Output: true
+ *
+ * Example 2:
+ * Input: root = [1,2,2,null,3,null,3]
+ * Output: false
+ */
+
+public class SymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+    private boolean isSymmetric(TreeNode nodeLeft, TreeNode nodeRight) {
+        if (nodeLeft == null && nodeRight == null) {
+            return true;
+        }
+        if (nodeLeft == null || nodeRight == null) {
+            return false;
+        }
+        return nodeLeft.val == nodeRight.val &&
+                isSymmetric(nodeLeft.left, nodeRight.right) &&
+                isSymmetric(nodeLeft.right, nodeRight.left);
+    }
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
