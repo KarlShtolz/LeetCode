7914380eb5f86542623fd0298f0addd1c7e202b8
leutnant <karl.shtolz.army@gmail.com>
2023-10-14 10:28:35 +0300

    120. Triangle

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,12 +3,12 @@ package org.example;
 import org.example.solutions.JumpGameII;
 import org.example.solutions.PascalsTriangle;
 import org.example.solutions.TrappingRainWater;
+import org.example.solutions.Triangle;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        PascalsTriangle pascalsTriangle = new PascalsTriangle();
-        System.out.println(pascalsTriangle.generate(3));
+
     }
 }
