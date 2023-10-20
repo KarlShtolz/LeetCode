9cbf14e9b767778702c986d5774c761f111f9617
leutnant <karl.shtolz.army@gmail.com>
2023-10-20 18:29:14 +0300

    2843. Count Symmetric Integers

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,12 @@
 package org.example;
 
-import org.example.solutions.JumpGameII;
-import org.example.solutions.PascalsTriangle;
-import org.example.solutions.TrappingRainWater;
-import org.example.solutions.Triangle;
+import org.example.solutions.*;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-
+        CountSymmetricIntegers countSymmetricIntegers = new CountSymmetricIntegers();
+        System.out.println(countSymmetricIntegers.countSymmetricIntegers(1, 100));
     }
 }
