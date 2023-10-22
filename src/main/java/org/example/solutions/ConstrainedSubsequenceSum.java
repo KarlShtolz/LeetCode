b6a359bcf93cbf8ccee52931294b1e166a407487
leutnant <karl.shtolz.army@gmail.com>
2023-10-22 20:15:00 +0300

    1425. Constrained Subsequence Sum

---
+++ src/main/java/org/example/solutions/ConstrainedSubsequenceSum.java
@@ -0,0 +1,54 @@
+package org.example.solutions;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 1425. Constrained Subsequence Sum
+ * Given an integer array nums and an integer k, return the maximum sum of a non-empty subsequence of that array such
+ * that for every two consecutive integers in the subsequence, nums[i] and nums[j], where i < j, the condition
+ * j - i <= k is satisfied.
+ *
+ * A subsequence of an array is obtained by deleting some number of elements (can be zero) from the array, leaving the
+ * remaining elements in their original order.
+ *
+ * Example 1:
+ * Input: nums = [10,2,-10,5,20], k = 2
+ * Output: 37
+ * Explanation: The subsequence is [10, 2, 5, 20].
+ *
+ * Example 2:
+ * Input: nums = [-1,-2,-3], k = 1
+ * Output: -1
+ * Explanation: The subsequence must be non-empty, so we choose the largest number.
+ *
+ * Example 3:
+ * Input: nums = [10,-2,-10,-5,20], k = 2
+ * Output: 23
+ * Explanation: The subsequence is [10, -2, -5, 20].
+ */
+public class ConstrainedSubsequenceSum {
+    public int constrainedSubsetSum(int[] nums, int k) {
+        int maxSum = nums[0];
+        Deque<Integer> maxSumQueue = new ArrayDeque<>();
+
+        for (int i = 0; i < nums.length; ++i) {
+            nums[i] += !maxSumQueue.isEmpty() ? maxSumQueue.peek() : 0;
+            maxSum = Math.max(maxSum, nums[i]);
+
+            while (!maxSumQueue.isEmpty() && nums[i] > maxSumQueue.peekLast()) {
+                maxSumQueue.pollLast();
+            }
+
+            if (nums[i] > 0) {
+                maxSumQueue.offer(nums[i]);
+            }
+
+            if (i >= k && !maxSumQueue.isEmpty() && maxSumQueue.peek() == nums[i - k]) {
+                maxSumQueue.poll();
+            }
+        }
+
+        return maxSum;
+    }
+}
