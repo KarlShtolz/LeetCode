e7ee1fa49ca2df28671d6d462f87ef7b0b0e7fab
leutnant-kata2 <karl.shtolz.army@gmail.com>
2024-05-19 15:40:42 +0300

    JumpGame added

---
+++ src/main/java/org/example/solutions/N_Queens.java
@@ -0,0 +1,71 @@
+package org.example.solutions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 51. N-Queens
+ * The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each
+ * other. Given an integer n, return all distinct solutions to the n-queens puzzle. You may return the answer
+ * in any order. Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.'
+ * both indicate a queen and an empty space, respectively.
+ *
+ * Input: n = 4
+ * Output: [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+ * Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above
+ *
+ * Input: n = 1
+ * Output: [["Q"]]
+ */
+public class N_Queens {
+    public boolean isSafe(int row,int col,int grid[][],int n){
+        for(int i=0;i<col;i++){
+            if(grid[row][i]==1) return false;
+        }
+
+        for(int i=row-1,j=col-1;i>=0&&j>=0;i--,j--){
+            if(grid[i][j]==1){
+                return false;
+            }
+        }
+        for(int i=row+1,j=col-1;i<n&&j>=0;i++,j--){
+            if(grid[i][j]==1){
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean solve(int col,int n,int grid[][],List<List<String>> ans){
+        if(col==n){
+            List<String> ar=new ArrayList<>();
+            for(int i=0;i<n;i++){
+                StringBuilder str=new StringBuilder("");
+                for(int j=0;j<n;j++){
+                    if(grid[i][j]==1)str.append("Q");
+                    else str.append(".");
+
+                }
+                ar.add(new String(str));
+            }
+            ans.add(new ArrayList<String>(ar));
+            return false;
+        }
+        for(int i=0;i<n;i++){
+            if(isSafe(i,col,grid,n)){
+                grid[i][col]=1;
+                if(solve(col+1,n,grid,ans)){
+                    return true;
+                }
+                grid[i][col]=0;
+            }
+        }
+        return false;
+
+    }
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> ans=new ArrayList<>();
+        int grid[][]=new int[n][n];
+        solve(0,n,grid,ans);
+        return ans;
+    }
+}
---
+++ src/main/java/org/example/solutions/N_QueensII.java
@@ -0,0 +1,41 @@
+package org.example.solutions;
+
+/**
+ * 52. N-Queens II
+ * The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
+ * Given an integer n, return the number of distinct solutions to the n-queens puzzle.
+ *
+ * Input: n = 4
+ * Output: 2
+ * Explanation: There are two distinct solutions to the 4-queens puzzle as shown.
+ *
+ * Input: n = 1
+ * Output: 1
+ */
+public class N_QueensII {
+    int count = 0;
+    public void total(int row ,int n , boolean[] cols,boolean[] nDiagnol,boolean[] rDiagnol){
+        if(row==n){
+            count++;
+            return;
+        }
+        for(int col=0;col<n;col++){
+            if(!cols[col] && !nDiagnol[row+col] && !rDiagnol[row-col+n-1]){
+                cols[col]=true;
+                nDiagnol[row+col]=true;
+                rDiagnol[row-col+n-1]=true;
+                total(row+1,n,cols,nDiagnol,rDiagnol);
+                cols[col]=false;
+                nDiagnol[row+col]=false;
+                rDiagnol[row-col+n-1]=false;
+            }
+        }
+    }
+    public int totalNQueens(int n) {
+        boolean[] cols = new boolean[n];
+        boolean[] nDiagnol = new boolean[2*n-1];
+        boolean[] rDiagnol = new boolean[2*n-1];
+        total(0,n,cols,nDiagnol,rDiagnol);
+        return count;
+    }
+}
