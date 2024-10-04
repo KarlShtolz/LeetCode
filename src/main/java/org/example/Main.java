e936d84a449c6071e57f3fb068e5906a213606f1
leutnant-kata2 <karl.shtolz.army@gmail.com>
2024-10-04 18:15:03 +0300

    62. Unique Paths

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,12 @@
 package org.example;
 
+import org.example.solutions.UniquePaths;
+
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        consoleTimer(50);
+        //consoleTimer(50);
+        UniquePaths uniquePaths = new UniquePaths();
+        System.out.println(uniquePaths.uniquePaths(3, 3));
     }
 
     /**
