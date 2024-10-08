359b6f471982a7872a4513731487b0c7733bea25
leutnant-kata2 <karl.shtolz.army@gmail.com>
2024-10-08 13:21:05 +0300

    231. Power of Two

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,22 @@
 package org.example;
 
+import org.example.solutions.PowerOfTwo;
+
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        consoleTimer(50);
+        //consoleTimer(50);
+        PowerOfTwo powerOfTwo = new PowerOfTwo();
+        System.out.println("0 " + powerOfTwo.isPowerOfTwo(0));
+        System.out.println("1 " + powerOfTwo.isPowerOfTwo(1));
+        System.out.println("2 " + powerOfTwo.isPowerOfTwo(2));
+        System.out.println("3 " + powerOfTwo.isPowerOfTwo(3));
+        System.out.println("4 " + powerOfTwo.isPowerOfTwo(4));
+        System.out.println("5 " + powerOfTwo.isPowerOfTwo(5));
+        System.out.println("6 " + powerOfTwo.isPowerOfTwo(6));
+        System.out.println("7 " + powerOfTwo.isPowerOfTwo(7));
+        System.out.println("8 " + powerOfTwo.isPowerOfTwo(8));
+        System.out.println("125 " + powerOfTwo.isPowerOfTwo(125));
+        System.out.println("256 " + powerOfTwo.isPowerOfTwo(256));
     }
     /**
      * @author Alexandr
