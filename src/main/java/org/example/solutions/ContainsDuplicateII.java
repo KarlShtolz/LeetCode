48ef25e54fbe90520802d5461641c5eead02069d
leutnant-kata2 <karl.shtolz.army@gmail.com>
2024-10-19 00:07:58 +0300

    219. Contains Duplicate II

---
+++ src/main/java/org/example/solutions/ContainsDuplicateII.java
@@ -0,0 +1,39 @@
+package org.example.solutions;
+
+import java.util.HashMap;
+
+/**
+ * 219. Contains Duplicate II
+ * Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array
+ * such that nums[i] == nums[j] and abs(i - j) <= k.
+ *
+ * Example 1:
+ * Input: nums = [1,2,3,1], k = 3
+ * Output: true
+ *
+ * Example 2:
+ * Input: nums = [1,0,1,1], k = 1
+ * Output: true
+ *
+ * Example 3:
+ * Input: nums = [1,2,3,1,2,3], k = 2
+ * Output: false
+ */
+public class ContainsDuplicateII {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(nums[i])){
+                int index = map.get(nums[i]);
+                if ((i - index) <= k) {
+                    return true;
+                } else {
+                    map.put(nums[i], i);
+                }
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return false;
+    }
+}
