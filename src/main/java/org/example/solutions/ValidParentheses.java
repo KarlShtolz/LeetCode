84bb73fec222d8d86e9d008df9d50009904f0f14
leutnant-kata2 <karl.shtolz.army@gmail.com>
2024-10-19 10:07:58 +0300

    20. Valid Parentheses add solution with Stack

--- src/main/java/org/example/solutions/ValidParentheses.java
+++ src/main/java/org/example/solutions/ValidParentheses.java
@@ -1,5 +1,7 @@
 package org.example.solutions;
 
+import java.util.Stack;
+
 /**
  * 20. Valid Parentheses
  * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
@@ -21,6 +23,12 @@ package org.example.solutions;
  * Output: false
  */
 public class ValidParentheses {
+    /**
+     * no Stack
+     * using Pointer
+     * @param s
+     * @return
+     */
     public boolean isValid(String s) {
         if (s.length() % 2 != 0) {
             return false;
@@ -75,6 +83,60 @@ public class ValidParentheses {
             }
         }
         if (pointer != 0) {
+            return false;
+        }
+        return true;
+    }
+    /**
+     * using Stack
+     * @param s
+     * @return
+     */
+    public boolean isValidStack(String s) {
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+        char [] arr = s.toCharArray();
+        if (arr[0] == ')' || arr[0] == ']' || arr[0] == '}') {
+            return false;
+        }
+        Stack<Character> st = new Stack<Character>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == '(' || arr[i] == '[' || arr[i] == '{') {
+                st.push(arr[i]);
+            }
+            if (arr[i] == ')') {
+                if (st.empty()) {
+                    return false;
+                }
+                if (st.peek() == '(') {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+            if (arr[i] == ']') {
+                if (st.empty()) {
+                    return false;
+                }
+                if (st.peek() == '[') {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+            if (arr[i] == '}') {
+                if (st.empty()) {
+                    return false;
+                }
+                if (st.peek() == '{') {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        if (st.empty() == false) {
             return false;
         }
         return true;
