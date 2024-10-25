f193d6f250c6d4b712e877d9486cc3eb1ede0cec
leutnant-kata2 <karl.shtolz.army@gmail.com>
2024-10-25 15:30:10 +0300

    PascalsTriangle nonRecursive

--- src/main/java/org/example/solutions/PascalsTriangle.java
+++ src/main/java/org/example/solutions/PascalsTriangle.java
@@ -36,4 +36,24 @@ public class PascalsTriangle {
         answer.add(tmp);
         return answer;
     }
+    public List<List<Integer>> generateNoRecursion(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (numRows == 0) {
+            return result;
+        }
+        List<Integer> firstRow = new ArrayList<>();
+        firstRow.add(1);
+        result.add(firstRow);
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> prevRow = result.get(i - 1);
+            List<Integer> currentRow = new ArrayList<>();
+            currentRow.add(1);
+            for (int j = 1; j < i; j++) {
+                currentRow.add(prevRow.get(j - 1) + prevRow.get(j));
+            }
+            currentRow.add(1);
+            result.add(currentRow);
+        }
+        return result;
+    }
 }
