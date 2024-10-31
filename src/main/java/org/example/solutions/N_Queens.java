112ace1660597063845be3ab6161078d2de449f4
leutnant-kata2 <karl.shtolz.army@gmail.com>
2024-10-31 20:24:44 +0300

    51. N-Queens refactoring

--- src/main/java/org/example/solutions/N_Queens.java
+++ src/main/java/org/example/solutions/N_Queens.java
@@ -18,54 +18,51 @@ import java.util.List;
  * Output: [["Q"]]
  */
 public class N_Queens {
-    public boolean isSafe(int row,int col,int grid[][],int n){
-        for(int i=0;i<col;i++){
-            if(grid[row][i]==1) return false;
+    public boolean isSafe (int row, int col, int[][] grid, int n) {
+        for (int i = 0; i < col; i++) {
+            if(grid[row][i] == 1) return false;
         }
-
-        for(int i=row-1,j=col-1;i>=0&&j>=0;i--,j--){
-            if(grid[i][j]==1){
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (grid[i][j] == 1) {
                 return false;
             }
         }
-        for(int i=row+1,j=col-1;i<n&&j>=0;i++,j--){
-            if(grid[i][j]==1){
+        for (int i = row + 1, j = col - 1; i < n && j >= 0; i++, j--) {
+            if (grid[i][j] == 1) {
                 return false;
             }
         }
         return true;
     }
-    public boolean solve(int col,int n,int grid[][],List<List<String>> ans){
-        if(col==n){
-            List<String> ar=new ArrayList<>();
-            for(int i=0;i<n;i++){
-                StringBuilder str=new StringBuilder("");
-                for(int j=0;j<n;j++){
-                    if(grid[i][j]==1)str.append("Q");
+    public boolean solve(int col, int n, int[][] grid, List<List<String>> ans) {
+        if (col == n) {
+            List<String> ar = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                StringBuilder str = new StringBuilder("");
+                for (int j = 0; j < n; j++) {
+                    if(grid[i][j] == 1) str.append("Q");
                     else str.append(".");
-
                 }
                 ar.add(new String(str));
             }
             ans.add(new ArrayList<String>(ar));
             return false;
         }
-        for(int i=0;i<n;i++){
-            if(isSafe(i,col,grid,n)){
-                grid[i][col]=1;
-                if(solve(col+1,n,grid,ans)){
+        for (int i = 0; i < n; i++) {
+            if (isSafe(i, col, grid, n)) {
+                grid[i][col] = 1;
+                if (solve(col+1, n, grid, ans)) {
                     return true;
                 }
-                grid[i][col]=0;
+                grid[i][col] = 0;
             }
         }
         return false;
-
     }
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> ans=new ArrayList<>();
-        int grid[][]=new int[n][n];
-        solve(0,n,grid,ans);
+        List<List<String>> ans = new ArrayList<>();
+        int[][] grid = new int[n][n];
+        solve(0, n, grid, ans);
         return ans;
     }
 }
