a50a977d9c1fc7b330fb4bc8d65950fd8f0a04b6
leutnant-kata2 <karl.shtolz.army@gmail.com>
2024-10-31 20:33:54 +0300

    2251. Number of Flowers in Full Bloom refactoring

--- src/main/java/org/example/solutions/NumberofFlowersinFullBloom.java
+++ src/main/java/org/example/solutions/NumberofFlowersinFullBloom.java
@@ -23,73 +23,53 @@ import java.util.Arrays;
  * For each person, we return the number of flowers in full bloom during their arrival.
  */
 public class NumberofFlowersinFullBloom {
-    public int[] fullBloomFlowers(int[][] flowers, int[] people) {
+    public int[] fullBloomFlowers(int [][] flowers, int [] people) {
         int m = flowers.length;
         int[] starts = new int[m];
         int[] ends = new int[m];
-        for(int i=0;i<m;i++)
-        {
-            starts[i]=flowers[i][0];
-            ends[i]= flowers[i][1];
+        for (int i = 0; i < m; i++) {
+            starts[i] = flowers[i][0];
+            ends[i] = flowers[i][1];
         }
         Arrays.sort(starts);
         Arrays.sort(ends);
         int n = people.length;
         int[] result = new int[n];
-
-        for(int i=0;i<n;i++)
-        {
-            int open =0, close=0;
-            if(people[i]<starts[0])
-            {
-                open =0;
-            }
-            else if(people[i]>=starts[m-1])
-            {
+        for (int i = 0; i < n; i++) {
+            int open = 0, close = 0;
+            if (people[i] < starts[0]) {
+                open = 0;
+            } else if (people[i] >= starts[m - 1]) {
                 open = m;
-            }
-            else
-            {
-                int l =0, r=m-1;
-                while(l<r)
-                {
-                    int mid = (l+r+1)>>1;
-                    if(starts[mid]<=people[i])
-                    {
+            } else {
+                int l = 0, r = m - 1;
+                while (l < r) {
+                    int mid = (l + r + 1) >> 1;
+                    if (starts[mid] <= people[i]) {
                         l = mid;
-                    }
-                    else
-                    {
-                        r=mid-1;
+                    } else {
+                        r = mid - 1;
                     }
                 }
-                open = l+1;
+                open = l + 1;
             }
-            if(people[i]<=ends[0])
+            if (people[i] <= ends[0]) {
                 close =0;
-            else if(people[i]>ends[m-1])
-            {
-                close =m;
-            }
-            else
-            {
-                int l=0, r=m-1;
-                while(l<r)
-                {
-                    int mid = (l+r+1)>>1;
-                    if(ends[mid]<people[i])
-                    {
-                        l=mid;
-
-                    }
-                    else
-                    {
-                        r = mid-1;
+            } else if (people[i] > ends[m - 1]) {
+                close = m;
+            } else {
+                int l = 0, r = m - 1;
+                while(l < r) {
+                    int mid = (l + r + 1) >> 1;
+                    if (ends[mid] < people[i]) {
+                        l = mid;
+                    } else {
+                        r = mid - 1;
                     }
                 }
-                close = l+1;
+                close = l + 1;
             }
-            result[i]=open-close;
+            result[i] = open - close;
         }
         return result;
     }
