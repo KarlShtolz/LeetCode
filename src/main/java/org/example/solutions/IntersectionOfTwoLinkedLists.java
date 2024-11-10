3b0c2de10f7d45ce83b8e586471c934354781b53
leutnant-kata2 <karl.shtolz.army@gmail.com>
2024-11-10 17:04:43 +0300

    108. Convert Sorted Array to Binary Search Tree

--- src/main/java/org/example/solutions/IntersectionOfTwoLinkedLists.java
+++ src/main/java/org/example/solutions/IntersectionOfTwoLinkedLists.java
@@ -8,7 +8,6 @@ import org.example.solutions.utils.ListNode;
  * two linked lists have no intersection at all, return null.
  *
  * Example 1:
- *
  * Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
  * Output: Intersected at '8'
  * Explanation: The intersected node's value is 8 (note that this must not be 0 if the two lists intersect).
