84c0fdbd279a90e57a43953531c6291df6dd5014
leutnant-kata2 <karl.shtolz.army@gmail.com>
2024-11-15 14:12:33 +0300

    405. Convert a Number to Hexadecimal

---
+++ src/main/java/org/example/solutions/ConvertANumberToHexadecimal.java
@@ -0,0 +1,23 @@
+package org.example.solutions;
+
+/**
+ * 405. Convert a Number to Hexadecimal
+ * Given a 32-bit integer num, return a string representing its hexadecimal representation. For negative integers,
+ * two’s complement method is used.
+ * All the letters in the answer string should be lowercase characters, and there should not be any leading zeros in
+ * the answer except for the zero itself.
+ * Note: You are not allowed to use any built-in library method to directly solve this problem.
+ *
+ * Example 1:
+ * Input: num = 26
+ * Output: "1a"
+ *
+ * Example 2:
+ * Input: num = -1
+ * Output: "ffffffff"
+ */
+public class ConvertANumberToHexadecimal {
+    public String toHex(int num) {
+        return Integer.toHexString(num);
+    }
+}
