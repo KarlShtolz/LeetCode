2bf03b87fa117180697e7b12148a7c7ce5e8c53e
leutnant-kata2 <karl.shtolz.army@gmail.com>
2024-11-16 18:58:11 +0300

    796. Rotate String

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,31 +5,7 @@ import org.example.solutions.*;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args)   {
 
-    }
-    /**
-     * @author Alexandr
-     * @version 1
-     * <pre>{@code
-     *      Main.consoleTimer(50);
-     * }</>
-     * метод выводит последней строкой консоли таймер,
-     * при увеличении значения старое значение удаляется
-     * и заменяется новым
-     * @param countSeconds
-     * @throws InterruptedException
-     */
-    public static void consoleTimer (int countSeconds) throws InterruptedException {
-        String tmp = "";
-        int lenString;
-        StringBuilder sb = null;
-        for (int i = 0; i < countSeconds; i++) {
-            tmp = String.valueOf(i);
-            lenString = tmp.length();
-            System.out.print(tmp);
-            Thread.sleep(1000);
-            System.out.print("\b".repeat(lenString));
-        }
     }
 }
