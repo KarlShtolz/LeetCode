6445a92caa4f10f61e41837fd8c9429946d13122
leutnant-kata2 <karl.shtolz.army@gmail.com>
2024-11-16 16:00:33 +0300

    747. Largest Number At Least Twice of Others

---
+++ src/main/java/org/example/solutions/LargestNumberAtLeastTwiceOfOthers.java
@@ -0,0 +1,39 @@
+package org.example.solutions;
+
+/**
+ * 747. Largest Number At Least Twice of Others
+ * You are given an integer array nums where the largest integer is unique.
+ * Determine whether the largest element in the array is at least twice as much as every other number in the array.
+ * If it is, return the index of the largest element, or return -1 otherwise.
+ *
+ * Example 1:
+ * Input: nums = [3,6,1,0]
+ * Output: 1
+ * Explanation: 6 is the largest integer.
+ * For every other number in the array x, 6 is at least twice as big as x.
+ * The index of value 6 is 1, so we return 1.
+ *
+ * Example 2:
+ * Input: nums = [1,2,3,4]
+ * Output: -1
+ * Explanation: 4 is less than twice the value of 3, so we return -1.
+ */
+public class LargestNumberAtLeastTwiceOfOthers {
+    int MAX = Integer.MIN_VALUE;
+    int MAX_INDEX = -1;
+    int ptr = 0;
+    public int dominantIndex(int[] nums) {
+        for (ptr = 0; ptr < nums.length; ptr++) {
+            if (nums[ptr] > MAX) {
+                MAX = nums[ptr];
+                MAX_INDEX = ptr;
+            }
+        }
+        for (ptr = 0; ptr < nums.length; ptr++) {
+            if (ptr != MAX_INDEX && MAX < nums[ptr] * 2) {
+                return -1;
+            }
+        }
+        return MAX_INDEX;
+    }
+}
