a01f0d083af909e62a31794f47af597e277e1ec0
leutnant-kata2 <karl.shtolz.army@gmail.com>
2025-11-16 19:52:54 +0300

    1513. Number of Substrings With Only 1s

---
+++ src/main/java/org/example/solutions/NumberOfSubstringsWithOnly1s.java
@@ -0,0 +1,40 @@
+package org.example.solutions;
+
+/**
+ * 1513. Number of Substrings With Only 1s
+ * Given a binary string s, return the number of substrings with all characters 1's. Since the answer may be too large,
+ * return it modulo 109 + 7.
+ *
+ * Example 1:
+ * Input: s = "0110111"
+ * Output: 9
+ * Explanation: There are 9 substring in total with only 1's characters.
+ * "1" -> 5 times.
+ * "11" -> 3 times.
+ * "111" -> 1 time.
+ *
+ * Example 2:
+ * Input: s = "101"
+ * Output: 2
+ * Explanation: Substring "1" is shown 2 times in s.
+ *
+ * Example 3:
+ * Input: s = "111111"
+ * Output: 21
+ * Explanation: Each substring contains only 1's characters.
+ */
+public class NumberOfSubstringsWithOnly1s {
+    public int numSub(String s) {
+        int mod = 1000000007;
+        int onesStreak = 0 ;
+        int res = 0;
+        for(char ch : s.toCharArray()) {
+            if (ch  == '1') {
+                res = ( res + ++onesStreak) % mod ;
+            } else {
+                onesStreak = 0 ;
+            }
+        }
+        return res;
+    }
+}
