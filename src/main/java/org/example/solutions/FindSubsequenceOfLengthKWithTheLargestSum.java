1d59ba4d0e593862b39ebc77323b723e99cf275a
leutnant-kata2 <karl.shtolz.army@gmail.com>
2025-11-20 12:56:16 +0300

    2099. Find Subsequence of Length K With the Largest Sum

---
+++ src/main/java/org/example/solutions/FindSubsequenceOfLengthKWithTheLargestSum.java
@@ -0,0 +1,51 @@
+package org.example.solutions;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * 2099. Find Subsequence of Length K With the Largest Sum
+ * You are given an integer array nums and an integer k. You want to find a subsequence of nums of length k that has
+ * the largest sum.
+ * Return any such subsequence as an integer array of length k.
+ * A subsequence is an array that can be derived from another array by deleting some or no elements without changing
+ * the order of the remaining elements.
+ *
+ * Example 1:
+ * Input: nums = [2,1,3,3], k = 2
+ * Output: [3,3]
+ * Explanation:
+ * The subsequence has the largest sum of 3 + 3 = 6.
+ *
+ * Example 2:
+ * Input: nums = [-1,-2,3,4], k = 3
+ * Output: [-1,3,4]
+ * Explanation:
+ * The subsequence has the largest sum of -1 + 3 + 4 = 6.
+ *
+ * Example 3:
+ * Input: nums = [3,4,3,3], k = 2
+ * Output: [3,4]
+ * Explanation:
+ * The subsequence has the largest sum of 3 + 4 = 7.
+ * Another possible subsequence is [4, 3].
+ */
+public class FindSubsequenceOfLengthKWithTheLargestSum {
+    public int[] maxSubsequence(int[] nums, int k) {
+        if (k == nums.length) {
+            return nums;
+        }
+        int [] ans = new int[k];
+        ArrayList<Integer> t = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            t.add(nums[i]);
+        }
+        for (int i = 0; i < nums.length - k; i++) {
+            t.remove(Collections.min(t));
+        }
+        for (int i = 0; i < k; i++) {
+            ans[i] = t.get(i);
+        }
+        return ans;
+    }
+}
