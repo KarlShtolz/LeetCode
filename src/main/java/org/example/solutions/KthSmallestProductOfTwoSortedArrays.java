6ebdbf4d861628549392bd3fda19af9abddb2538
leutnant-kata2 <karl.shtolz.army@gmail.com>
2025-11-20 22:39:06 +0300

    2040. Kth Smallest Product of Two Sorted Arrays

---
+++ src/main/java/org/example/solutions/KthSmallestProductOfTwoSortedArrays.java
@@ -0,0 +1,71 @@
+package org.example.solutions;
+
+/**
+ * 2040. Kth Smallest Product of Two Sorted Arrays
+ * Given two sorted 0-indexed integer arrays nums1 and nums2 as well as an integer k, return the kth (1-based) smallest
+ * product of nums1[i] * nums2[j] where 0 <= i < nums1.length and 0 <= j < nums2.length.
+ *
+ * Example 1:
+ * Input: nums1 = [2,5], nums2 = [3,4], k = 2
+ * Output: 8
+ * Explanation: The 2 smallest products are:
+ * - nums1[0] * nums2[0] = 2 * 3 = 6
+ * - nums1[0] * nums2[1] = 2 * 4 = 8
+ * The 2nd smallest product is 8.
+ *
+ * Example 2:
+ * Input: nums1 = [-4,-2,0,3], nums2 = [2,4], k = 6
+ * Output: 0
+ * Explanation: The 6 smallest products are:
+ * - nums1[0] * nums2[1] = (-4) * 4 = -16
+ * - nums1[0] * nums2[0] = (-4) * 2 = -8
+ * - nums1[1] * nums2[1] = (-2) * 4 = -8
+ * - nums1[1] * nums2[0] = (-2) * 2 = -4
+ * - nums1[2] * nums2[0] = 0 * 2 = 0
+ * - nums1[2] * nums2[1] = 0 * 4 = 0
+ * The 6th smallest product is 0.
+ *
+ * Example 3:
+ * Input: nums1 = [-2,-1,0,1,2], nums2 = [-3,-1,2,4,5], k = 3
+ * Output: -6
+ * Explanation: The 3 smallest products are:
+ * - nums1[0] * nums2[4] = (-2) * 5 = -10
+ * - nums1[0] * nums2[3] = (-2) * 4 = -8
+ * - nums1[4] * nums2[0] = 2 * (-3) = -6
+ * The 3rd smallest product is -6.
+ */
+public class KthSmallestProductOfTwoSortedArrays {
+    public long kthSmallestProduct(int[] nums1, int[] nums2, long k) {
+        long left = -10000000000L;
+        long right = 10000000000L;
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            long count = 0;
+            for (int i = 0; i < nums1.length; i++) {
+                count += countProducts(nums2, nums1[i], mid);
+            }
+            if (count < k) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+    public long countProducts(int[] arr, int val, long product) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long currentProduct = (long) val * (long) arr[mid];
+            if ((val >= 0 && currentProduct <= product) || (val < 0 && currentProduct > product)) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        if (val >= 0)
+            return left;
+        return arr.length - left;
+    }
+}
