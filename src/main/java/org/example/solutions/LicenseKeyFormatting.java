593e43e11aad5f6c7e385472ebb26066f666dba8
leutnant-kata2 <karl.shtolz.army@gmail.com>
2025-11-21 00:40:49 +0300

    482. License Key Formatting

---
+++ src/main/java/org/example/solutions/LicenseKeyFormatting.java
@@ -0,0 +1,58 @@
+package org.example.solutions;
+
+/**
+ * 482. License Key Formatting
+ * You are given a license key represented as a string s that consists of only alphanumeric characters and dashes.
+ * The string is separated into n + 1 groups by n dashes. You are also given an integer k.
+ * We want to reformat the string s such that each group contains exactly k characters, except for the first group,
+ * which could be shorter than k but still must contain at least one character. Furthermore, there must be a dash
+ * inserted between two groups, and you should convert all lowercase letters to uppercase.
+ * Return the reformatted license key.
+ *
+ * Example 1:
+ * Input: s = "5F3Z-2e-9-w", k = 4
+ * Output: "5F3Z-2E9W"
+ * Explanation: The string s has been split into two parts, each part has 4 characters.
+ * Note that the two extra dashes are not needed and can be removed.
+ *
+ * Example 2:
+ * Input: s = "2-5g-3-J", k = 2
+ * Output: "2-5G-3J"
+ * Explanation: The string s has been split into three parts, each part has 2 characters except the first part as it
+ * could be shorter as mentioned above.
+ */
+public class LicenseKeyFormatting {
+    public String licenseKeyFormatting(String s, int k) {
+        if (s.equals("-")) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        String t = "";
+        int tmp = k - 1;
+        for (int i = s.length() - 1; i >= 0 ; i--) {
+
+            if (s.charAt(i) == '-') {
+                continue;
+            }
+            if (Character.isLetter(s.charAt(i))) {
+                t = s.charAt(i) + "";
+                sb.append(t.toUpperCase());
+            } else {
+                sb.append(s.charAt(i));
+            }
+            if (tmp == 0 && i != 0) {
+                tmp = k - 1;
+                sb.append("-");
+            } else {
+                tmp--;
+            }
+        }
+        String ans = sb.reverse().toString();
+        if (ans.length() != 0) {
+            if (ans.charAt(0) == '-') {
+                ans = ans.substring(1);
+            }
+        }
+        return ans;
+    }
+}
