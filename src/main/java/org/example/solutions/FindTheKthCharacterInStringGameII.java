8f2cff1c9b013a18ae491b2428b4c78da35d743e
leutnant-kata2 <karl.shtolz.army@gmail.com>
2025-11-20 22:46:27 +0300

    3307. Find the K-th Character in String Game II

---
+++ src/main/java/org/example/solutions/FindTheKthCharacterInStringGameII.java
@@ -0,0 +1,54 @@
+package org.example.solutions;
+
+/**
+ * 3307. Find the K-th Character in String Game II
+ * Alice and Bob are playing a game. Initially, Alice has a string word = "a".
+ * You are given a positive integer k. You are also given an integer array operations, where operations[i] represents
+ * the type of the ith operation.
+ * Now Bob will ask Alice to perform all operations in sequence:
+ * If operations[i] == 0, append a copy of word to itself.
+ * If operations[i] == 1, generate a new string by changing each character in word to its next character in the English
+ * alphabet, and append it to the original word. For example, performing the operation on "c" generates "cd" and
+ * performing the operation on "zb" generates "zbac".
+ * Return the value of the kth character in word after performing all the operations.
+ * Note that the character 'z' can be changed to 'a' in the second type of operation.
+ *
+ * Example 1:
+ * Input: k = 5, operations = [0,0,0]
+ * Output: "a"
+ * Explanation:
+ * Initially, word == "a". Alice performs the three operations as follows:
+ * Appends "a" to "a", word becomes "aa".
+ * Appends "aa" to "aa", word becomes "aaaa".
+ * Appends "aaaa" to "aaaa", word becomes "aaaaaaaa".
+ *
+ * Example 2:
+ * Input: k = 10, operations = [0,1,0,1]
+ * Output: "b"
+ * Explanation:
+ * Initially, word == "a". Alice performs the four operations as follows:
+ * Appends "a" to "a", word becomes "aa".
+ * Appends "bb" to "aa", word becomes "aabb".
+ * Appends "aabb" to "aabb", word becomes "aabbaabb".
+ * Appends "bbccbbcc" to "aabbaabb", word becomes "aabbaabbbbccbbcc".
+ */
+public class FindTheKthCharacterInStringGameII {
+    public char kthCharacter(long k, int[] operations) {
+        long jump = 0, num = k;
+        while (num > 1) {
+            int lo_range = (int)(Math.log(num) / Math.log(2));
+            int hi_range = lo_range + 1;
+            long border = 1L << lo_range;
+            if (border == num) {
+                lo_range--;
+                border = 1L << lo_range;
+            }
+            int ind = lo_range;
+            num -= border;
+            if (operations[ind] == 1) {
+                jump++;
+            }
+        }
+        return (char)('a' + (int)(jump % 26));
+    }
+}
